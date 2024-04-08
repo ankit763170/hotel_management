74d1f826de979a112e2c568ea0294fc4495c8a43
ankit763170 <ankitjha@coderixx.com>
2024-04-08 09:18:13 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<!-- HTML -->
+<template>
+  <h1>Hello World this is checking</h1>
+</template>
+<!-- CSS -->
+<style>
+h1{
+color:  red;
+
+}
+</style>
+<!-- Javascript -->
+<script>
+console.log("hey this is console...")
+</script>
